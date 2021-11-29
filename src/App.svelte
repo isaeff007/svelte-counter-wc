1aef6e8c8f898ec4250750b6795cce03555a68e8
isaeff007 <isaeff007@gmail.com>
2021-11-29 14:46:37 +0100

    - counter button as counter-btn.js  after build (ready to integrate in Angular as JS script)

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<svelte:options tag="count-btn" />
+
+<script>
+  import { onMount } from "svelte";
+  export let count = 0;
+
+  function onClick() {
+    count = +count + 1;
+  }
+  onMount(() => {
+    console.log("can use on mount");
+  });
+</script>
+
+<button on:click={onClick}>Count {count}</button>
+
+<style>
+  button {
+    width: 100px;
+    height: 100px;
+  }
+</style>
